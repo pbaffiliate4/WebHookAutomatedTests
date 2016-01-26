cefde193bb15e420019caa05ea817fc546772d75
Afaf Zakhya <pbaffiliate4@gmail.com>
2016-01-26 09:46:48 +0200

    m3

---
+++ scripts/math3.js
@@ -0,0 +1,7 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="scriptr_return" id="33" inline="false" x="106" y="79"><value name="return"><block type="math_arithmetic" id="52" inline="true"><field name="OP">ADD</field><value name="A"><block type="math_number" id="120"><field name="NUM">7</field></block></value><value name="B"><block type="math_number" id="101"><field name="NUM">7</field></block></value></block></value></block></xml>*#*#*/
+return 7 + 7			
