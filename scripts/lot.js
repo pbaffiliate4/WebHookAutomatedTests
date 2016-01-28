09b9fddab77361e20ceee777881f23cb733cdf3a
Afaf Zakhya <pbaffiliate4@gmail.com>
2016-01-28 12:19:13 +0200

    Update lot.js

--- scripts/lot.js
+++ scripts/lot.js
@@ -25,7 +25,6 @@ function GoogleAnalyticsClient(tid) {
  * optionalParamsObj {object} optional parameters  Event label and Event value. Example : {"el":"holiday" // Event label ,"ev":"300"// Event value. ..... } or {} .
  * userID {String}  the user id 
  * cid {String}  client id or "" 
- * debug {boolean}  debug mode true for debug and false for no debug
  * @return in case of degug is true "  "hitParsingResult": [ {"valid": true,"parserMessage": [ ],"hit": "POST /debug/collect HTTP/1.1"} ]}". in case of debug false empty string.
  *Example:{"eventCategory":"scriptCall","eventAction":"action","optionalParamsObj":{},"userID":request.user.id,"cid":"35009a79-1a05-49d7-b876-2b884d0f825b","debug":true}
  */
@@ -39,12 +38,6 @@ GoogleAnalyticsClient.prototype.track = function(params) {
   	this.googleUrl=config.getGoogleAnalyticsUrl();  
   }
   
-  var cid;
-  if(params.cid == ""){
-     cid=params.userID;
-   }else{
-     cid=params.cid;
-   }
   var requestParams = {  
       "url": this.googleUrl,
       "params": {
@@ -55,7 +48,6 @@ GoogleAnalyticsClient.prototype.track = function(params) {
         "ec": params.eventCategory,
         "ea": params.eventAction
       },
-      "method": "POST"
   };
   for (var attrname in params.optionalParamsObj) { 
     	requestParams.params[attrname] = params.optionalParamsObj[attrname]; 
@@ -65,7 +57,6 @@ GoogleAnalyticsClient.prototype.track = function(params) {
 
 /**
  * issue an http request towards google analytics's APIs. 
- *Throws an exception if returned status code =/= 200
  * or =/= 307
  * @method _callApi
  * @return the body of the http response, as a JSON or String, depending on the response's content-type
@@ -93,13 +84,11 @@ GoogleAnalyticsClient.prototype._parseResponse = function(response) {
   }
   throw {
     "errorCode": "google_analytics_error",
-    "errorDetail": "Remote google analytics API returned an error " + response.status + " (" + JSON.stringify(response.body) + ")"
   } 
 };
 
 /**
  * @method _parseBody
- * @return the body of the http response, as a JSON or String, depending on the response's content-type
  */
 GoogleAnalyticsClient.prototype._parseBody = function(response) {
   var responseBodyStr = response.body;
@@ -108,4 +97,12 @@ GoogleAnalyticsClient.prototype._parseBody = function(response) {
   	}
   
   return responseBodyStr;
-};			
+}
+GoogleAnalyticsClient.prototype._parseBody = function(response) {
+  var responseBodyStr = response.body;
+  if (response.headers["Content-Type"].indexOf("application/json") > -1 && responseBodyStr!="") {
+    	return JSON.parse(responseBodyStr);
+  	}
+  
+  return responseBodyStr;
+};
