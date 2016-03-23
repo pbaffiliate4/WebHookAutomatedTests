f6cd7799bba71691649186668b4033fed78bee7b
Afaf Zakhya <pbaffiliate4@gmail.com>
2016-03-23 12:41:00 +0200

    bassem

---
+++ scripts/coco.js
@@ -0,0 +1,14 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=anonymous 
+  **/ 
+ var test = [ 
+    ['Language', 'Speakers (in millions)'],
+    ['German',  5.85],
+    ['French',  1.66],
+    ['Italian', 0.316],
+    ['Romansh', 0.0791]
+]
+
+return test;			
