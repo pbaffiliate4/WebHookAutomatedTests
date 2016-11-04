dce75a7a9dd72d7775c522cd26369fda496bbe7f
Afaf Zakhya <pbaffiliate4@gmail.com>
2016-11-04 12:13:17 +0200

    Delete jjjj.js
    @AUTO-DEPLOY

--- scripts/eeeee/nnnn/vvvv/jjjj.js
+++
@@ -1,7 +0,0 @@
-/** Script ACLs do not delete 
- read=nobody 
-write=nobody
-execute=authenticated 
-  **/ 
- //	return apsdb.callApi("ListConfiguration", null, null).result.defaultGitWebhookId;
-		return apsdb.callApi('ListConfiguration', null, null).result.gitPurge;			
