945829f454060fff56cf468533db359a08529c15
Afaf Zakhya <pbaffiliate4@gmail.com>
2016-11-04 11:56:17 +0200

    Delete zoro.js
    testing

--- scripts/zoro.js
+++
@@ -1,8 +0,0 @@
-/** Script ACLs do not delete 
- read=nobody 
-write=nobody
-execute=authenticated 
-  **/ 
- /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="scriptr_push_notification" id="1" inline="true" x="1" y="-118"><mutation isproduction="true"></mutation><field name="deviceType">ios</field><field name="isproduction">false</field></block></xml>*#*#*/
-push(null, null, "ios", "false");
-			
